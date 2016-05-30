a94a85ae27ec5dee49fc621c2d00b22753085d6f
zadochob <zadochob@gmail.com>
2016-05-30 11:42:38 +0200

    [wip] Integrating last changes in Application class

--- lib/Application.php
+++ lib/Application.php
@@ -3,20 +3,32 @@
 namespace Picon\Lib;
 
 use Picon\Lib\Config    as Config;
-use Picon\Lib\Exception as Exception_Handler;
 use Picon\Lib\Router    as Router;
+use Picon\Lib\Error     as Error;
 
 class Application{
 
     public      $config;
     public      $router;
+    public      $error;
 
     protected   $currentController;
 
     public function __construct($app_name = "Picon"){
         $this->config   =   new Config(); 
         $this->router   =   new Router();
+        $this->error    =   new Error();
         $route_infos    =   $this->router->route();
+        if($route_infos["controller"]   == "_error_")
+            throw new HttpException(404, $route_infos);
+    }
+
+    public function callControllerAction(){
+
+    }
+
+    public function callView(){
+
     }
 
 }
