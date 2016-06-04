5fa1fec3123f5cfa65c9f9e23b9567f4dc622cd9
FM@lilascat <emmanuel@octopuce.fr>
2016-06-04 17:06:47 +0200

    [Wip] Adding model connection part

--- lib/Model.php
+++ lib/Model.php
@@ -4,5 +4,31 @@ namespace Picon\Lib;
 
 class Model{
 
+    static protected $db;
+
+    public function __construct(){
+        $this->setConnection();
+    }
+
+    protected function setConnection($force = false){
+        $connectionInfos    =   Config::get_value("db");
+        $connectionString   =   "mysql:host=localhost;dbname=" . $connectionInfos["name"];
+        if(!isset($db) || $force){
+            try{
+                self::$db   =   new PDO($connectionString, $connectionInfos["user"], $connectionInfos["pass"]);
+            } catch (PDOException $e){
+                throw new HttpException(500, $e->getMessage());
+            }
+        }
+        return self::db;
+
+    }
+
+    protected function getConnection(){
+        if(!isset(self::$db)){
+            $this->setConnection();
+        }
+        return self::$db;
+    }
 
 }
