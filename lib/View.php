18f80091180b8ac98b293ce18bb4b0d59c791d7d
FM@lilascat <emmanuel@octopuce.fr>
2016-06-05 13:07:32 +0200

    [wip] Now we load vars in views

--- lib/View.php
+++ lib/View.php
@@ -14,6 +14,7 @@ class View{
         $this->layout           =   $viewInfos["layout"]; 
         $this->view             =   $viewInfos["view"];
         $this->vars             =   $vars;
+        $this->loadVars();
         $this->isRenderedLayout = false;
     }
 
